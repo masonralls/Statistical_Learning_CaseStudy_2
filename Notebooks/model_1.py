76d393baaa4cb5e79e8a23e85967ca6af0904b44
masonralls <masonralls@mail.weber.edu>
2025-11-06 14:16:58 -0700

    Add initial data visualization and time series cross-validation setup in model_1.py; create kaggle_data.txt for dataset path

--- Notebooks/model_1.py
+++ Notebooks/model_1.py
@@ -19,3 +19,32 @@ print("Path to dataset files:", path)
 # Display the first few rows of data
 df = pd.read_csv(path + "/Truck_sales.csv")
 df.head()
+
+# initial plot of sales over time
+df.plot(x = 'Month-Year', y = 'Number_Trucks_Sold',
+        style='.',
+        figsize=(15, 5),
+        color=color_pal()[0],
+        title='Number of Trucks Sold from 2003 to 2014')
+plt.xlabel('Month-Year')
+plt.ylabel('Number of Trucks Sold')
+
+# Time Series Cross Validation with plots
+tss = TimeSeriesSplit(n_splits=5, test_size=12, gap=1)
+df = df.sort_index()
+fig, axs = plt.subplots(5, 1, figsize=(15, 15), sharex=True)
+
+fold = 0
+for train_idx, val_idx in tss.split(df):
+    train = df.iloc[train_idx]
+    test = df.iloc[val_idx]
+    train['Number_Trucks_Sold'].plot(ax=axs[fold],
+                          label='Training Set',
+                          title=f'Data Train/Test Split Fold {fold}')
+    test['Number_Trucks_Sold'].plot(ax=axs[fold],
+                         label='Test Set')
+    axs[fold].axvline(test.index.min(), color='black', ls='--')
+
+    fold += 1
+plt.show()
+
